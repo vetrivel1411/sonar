40381ea5bb037d49b98c545421fcc01514cad4c4
vetrivel1411 <vetrivelifet22@gmail.com>
2025-05-07 13:54:36 +0530

    Update SonarqubeApplicationTests.java

--- src/test/java/com/example/sonarqube/SonarqubeApplicationTests.java
+++ src/test/java/com/example/sonarqube/SonarqubeApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.sonarqube;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class SonarqubeApplicationTests {
 
@@ -13,7 +15,6 @@ class SonarqubeApplicationTests {
 	@Test
 	void mainMethodShouldRunWithoutErrors() {
 		assertDoesNotThrow(() -> SonarqubeApplication.main(new String[]{}),
-			"Application should start without throwing exceptions");
+				"Application should start without throwing exceptions");
 	}
-
 }
